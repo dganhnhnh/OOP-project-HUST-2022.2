209ce9153bbdde873c1f0ff5166d96f0a6b1711e
duonganh <duonganh2973@gmail.com>
2023-05-07 21:36:15 +0700

    still returns errors, but can continue

---
+++ src/main/java/com/projectoop/services/JpaTableCheck.java
@@ -0,0 +1,15 @@
+package com.projectoop.services;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.metamodel.Metamodel;
+
+import com.projectoop.model.Question;
+
+import javax.persistence.metamodel.ManagedType;
+import javax.persistence.PersistenceUnitUtil;
+
+public class JpaTableCheck {
+    
+}
+
